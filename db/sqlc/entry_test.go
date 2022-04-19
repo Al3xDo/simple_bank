ed33a642ba5d902df696486fcf3b29f023a70318
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2022-04-19 19:12:01 +0700

    first commit

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createEntries(t *testing.T) Entry {
+	account := createRandomAccount(t)
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomAmount(account.Balance),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, entry.AccountID, account.ID)
+	require.Equal(t, entry.Amount, arg.Amount)
+	require.NotZero(t, entry.CreatedAt)
+	require.NotZero(t, entry.ID)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createEntries(t)
+}
+func TestGetEntry(t *testing.T) {
+	entry1 := createEntries(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+func TestGetListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createEntries(t)
+	}
+	arg := ListEntriessParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	entries, err := testQueries.ListEntriess(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
+
+func TestDeleteEntries(t *testing.T) {
+	entry1 := createEntries(t)
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,71 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomAmount(account1.Balance),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.Equal(t, transfer.Amount, arg.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+}
+func TestGetTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+func TestGetListTransfers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t)
+	}
+	arg := ListTransfersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
+
+func TestDeleteTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+	err := testQueries.DeleteTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, transfer2)
+}
