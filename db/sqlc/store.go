62dcca08a74b9e7ab9e95911d9af153fb957aa8b
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2022-07-10 14:03:23 +0700

    update

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,20 +7,26 @@ import (
 )
 
 // store provides all function to execute db queries and transaction
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLstore provides all function to execute db queries and transaction
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	//begin transaction
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -59,7 +65,7 @@ var txKey = struct{}{}
 // private function start with _
 // TransferTx perfoirms a money transfer from account to the other
 // It creates a transfer record -> add account entries -> update account's balance within a single transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
 		//create transfer record
